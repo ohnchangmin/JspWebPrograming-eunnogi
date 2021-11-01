68508d52903698796cf808209784b4543aab6227
ohnchangmin <ok_ddol@naver.com>
2021-11-01 23:52:25 +0900

    챕터13 게시판 자바빈 작성
    1. 글 내용 조회
    2. 글 수정
    3. 글 삭제

--- StudyBasicJSP/src/main/java/ch13/board/BoardDBBean.java
+++ StudyBasicJSP/src/main/java/ch13/board/BoardDBBean.java
@@ -167,4 +167,164 @@ public class BoardDBBean {
 		return articleList;
 	}
 	
-}
+	//글의 내용을 보기(1개의 글)(select문)<=content.jsp페이지에서 사용
+	public BoardDataBean getArticle(int num) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		BoardDataBean article = null;
+		
+		try {
+			conn = getConnection();
+			
+			pstmt = conn.prepareStatement("update board set readcount = readcount + 1 where num = ?");
+			pstmt.setInt(1, num);
+			pstmt.executeUpdate();
+			
+			pstmt = conn.prepareStatement("select * from board where num = ?");
+			pstmt.setInt(1, num);
+			rs =pstmt.executeQuery();
+			
+			if(rs.next()) {
+				article = new BoardDataBean();
+				article.setNum(rs.getInt("num"));
+				article.setWriter(rs.getString("writer"));
+				article.setEmail(rs.getString("email"));
+				article.setSubject(rs.getString("subject"));
+				article.setPasswd(rs.getString("passwd"));
+				article.setReg_date(rs.getTimestamp("reg_date"));
+				article.setReadcount(rs.getInt("readcount"));
+				article.setRef(rs.getInt("ref"));
+				article.setRe_step(rs.getInt("re_step"));
+				article.setRe_level(rs.getInt("re_level"));
+				article.setContent(rs.getString("content"));
+				article.setIp(rs.getString("ip"));
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}finally {
+			if(rs != null)try {rs.close();}catch(SQLException ex) {}
+			if(pstmt != null)try {pstmt.close();}catch(SQLException ex) {}
+			if(conn != null)try {conn.close();}catch(SQLException ex) {}
+		}
+		return article;
+	}
+	
+	//글수정 폼에서 사용할 글의 내용(1개의 글)(select문)<=updateForm.jsp에서 사용
+	public BoardDataBean updateGetArticle(int num) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		BoardDataBean article = null;
+		
+		try {
+			conn = getConnection();
+			
+			pstmt = conn.prepareStatement("select * from board where num = ?");
+			pstmt.setInt(1, num);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				article = new BoardDataBean();
+				article.setNum(rs.getInt("num"));
+				article.setWriter(rs.getString("writer"));
+				article.setEmail(rs.getString("email"));
+				article.setSubject(rs.getString("subject"));
+				article.setPasswd(rs.getString("passwd"));
+				article.setReg_date(rs.getTimestamp("reg_date"));
+				article.setReadcount(rs.getInt("readcount"));
+				article.setRef(rs.getInt("ref"));
+				article.setRe_step(rs.getInt("re_step"));
+				article.setRe_level(rs.getInt("re_level"));
+				article.setContent(rs.getString("content"));
+				article.setIp(rs.getString("ip"));
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}finally {
+			if(rs != null)try {rs.close();}catch(SQLException ex) {}
+			if(pstmt != null)try {pstmt.close();}catch(SQLException ex) {}
+			if(conn != null)try {conn.close();}catch(SQLException ex) {}
+		}
+		return article;
+	}
+	
+	//글수정 처리에서 사용(update문)<=updatePro.jsp에서 사용
+	public int updateArticle(BoardDataBean article) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt  =null;
+		ResultSet rs = null;
+		
+		String dbpasswd = null;
+		String sql = "";
+		int x = -1;
+		try {
+			conn = getConnection();
+			
+			pstmt = conn.prepareStatement("select passwd from board where num = ?");
+			pstmt.setInt(1, article.getNum());
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dbpasswd = rs.getString("passwd");
+				if(dbpasswd.equals(article.getPasswd())) {
+					sql = "update board set writer = ?, email = ?, subject = ?, passwd = ?, content = ? where num = ?";
+					pstmt = conn.prepareStatement(sql);
+					
+					pstmt.setString(1, article.getWriter());
+					pstmt.setString(2, article.getEmail());
+					pstmt.setString(3, article.getSubject());
+					pstmt.setString(4, article.getPasswd());
+					pstmt.setString(5, article.getContent());
+					pstmt.setInt(6, article.getNum());
+					pstmt.executeUpdate();
+					
+					x=1;
+				}else {
+					x=0;
+				}
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}finally {
+			if(rs != null)try {rs.close();}catch(SQLException ex) {}
+			if(pstmt != null)try {pstmt.close();}catch(SQLException ex) {}
+			if(conn != null)try {conn.close();}catch(SQLException ex) {}
+		}
+		return x;
+	}
+		
+		//글삭제처리시 사용(delete문)<=deletePro.jsp 페이지에서 사용
+		public int deleteArticle(int num, String passwd) throws Exception{
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			String dbpasswd = "";
+			int x = -1;
+			try {
+				conn = getConnection();
+				
+				pstmt = conn.prepareStatement("select passwd from board where num = ?");
+				pstmt.setInt(1, num);
+				rs = pstmt.executeQuery();
+				
+				if(rs.next()) {
+					dbpasswd = rs.getString("passwd");
+					if(dbpasswd.equals(passwd)) {
+						pstmt = conn.prepareStatement("delete from board where num = ?");
+						pstmt.setInt(1, num);
+						pstmt.executeUpdate();
+						x = 1; //글삭제 성공
+					}else
+						x = 0;
+				}
+			}catch(Exception ex) {
+				ex.printStackTrace();
+			}finally {
+				if(rs != null)try {rs.close();}catch(SQLException ex) {}
+				if(pstmt != null)try {pstmt.close();}catch(SQLException ex) {}
+				if(conn != null)try {conn.close();}catch(SQLException ex) {}
+			}
+			return x;
+		}
+	}
