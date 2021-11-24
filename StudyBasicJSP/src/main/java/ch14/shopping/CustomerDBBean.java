3c7c6373d5364ca0496f79a8de3272a03f38ca9a
ohnchangmin <ok_ddol@naver.com>
2021-11-24 22:42:26 +0900

    쇼핑 관련 데이터베이스 접근 객체 메소드 작성
    1. 사용자 인증처리
    2. 중복아이디 체크
    3. 회원정보 수정을 위한 기존 정보 표시
    4. 회원 정보 수정
    5. 회원 탈퇴

--- StudyBasicJSP/src/main/java/ch14/shopping/CustomerDBBean.java
+++ StudyBasicJSP/src/main/java/ch14/shopping/CustomerDBBean.java
@@ -2,6 +2,7 @@ package ch14.bookshop.shopping;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.naming.Context;
@@ -50,5 +51,169 @@ public class CustomerDBBean {
 			if(conn!=null)
 				try {conn.close();}catch(SQLException sqle){}
 		}
+	}
+	
+	/* 사용자 인증처리 */
+	public int userCheck(String id, String passwd) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String dbpasswd = "";
+		int x = -1;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement("select passwd from member where id = ?");
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dbpasswd = rs.getString("passwd");
+				if(dbpasswd.equals(passwd))
+					x = 1;
+				else
+					x = 0;
+			}else
+				x = -1;
+			}catch(Exception ex) {
+				ex.printStackTrace();
+			}finally {
+				if(rs!=null)
+					try {rs.close();}catch(SQLException sqle) {}
+				if(pstmt!=null)
+					try {pstmt.close();}catch(SQLException sqle) {}
+				if(conn!=null)
+					try {conn.close();}catch(SQLException sqle) {}
+			}
+		return x;
+	}
+	
+	/* 중복아이디 체크 */
+	public int confirmId(String id) throws Exception {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int x = -1;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement("select id from member where id = ?");
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next())
+				x = 1;
+			else
+				x = -1;
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}finally {
+			if(rs!=null)
+				try {rs.close();}catch(SQLException sqle) {}
+			if(pstmt!=null)
+				try {pstmt.close();}catch(SQLException sqle) {}
+			if(conn!=null)
+				try {conn.close();}catch(SQLException sqle) {}
+		}
+		return x;
+	}
+	
+	/* 회원정보를 수정하기 위해 기존의 정보를 표시 */
+	public CustomerDataBean getMember(String id) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		CustomerDataBean member = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement("select * from member where id = ?");
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				member = new CustomerDataBean();
+				
+				member.setId(rs.getString("id"));
+				member.setPasswd(rs.getString("passwd"));
+				member.setName(rs.getString("name"));
+				member.setReg_date(rs.getTimestamp("reg_date"));
+				member.setTel(rs.getString("tel"));
+				member.setAddress(rs.getString("address"));
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}finally {
+			if(rs!=null)
+				try {rs.close();}catch(SQLException sqle) {}
+			if(pstmt!=null)
+				try {pstmt.close();}catch(SQLException sqle) {}
+			if(conn!=null)
+				try {conn.close();}catch(SQLException sqle) {}
+		}
+		return member;
+	}
+	
+	/* 회원의 정보 수정 */
+	public void updateMember(CustomerDataBean member) throws Exception {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement("update member set passwd = ?, name = ?, tel = ?, address = ?, where id = ?");
+			pstmt.setString(1, member.getPasswd());
+			pstmt.setString(2, member.getName());
+			pstmt.setString(3, member.getTel());
+			pstmt.setString(4, member.getAddress());
+			pstmt.setString(5, member.getId());
+			
+			pstmt.executeUpdate();
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}finally {
+			if(pstmt!=null)
+				try {pstmt.close();}catch(SQLException sqle) {}
+			if(conn!=null)
+				try {conn.close();}catch(SQLException sqle) {}
+		}
+	}
+	
+	/* 회원 탈퇴 */
+	public int deleteMember(String id, String passwd) throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String dbpasswd = null;
+		int x = -1;
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement("select passwd from member where id = ?");
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				dbpasswd = rs.getString("passwd");
+				if(dbpasswd.equals(passwd)) {
+					pstmt = conn.prepareStatement("delete from member where id = ?");
+					pstmt.setString(1, id);
+					pstmt.executeUpdate();
+					x = 1;
+				}
+				else
+					x = 0;
+			}
+		}catch(Exception ex) {
+			ex.printStackTrace();
+		}finally {
+			if(rs!=null)
+				try {rs.close();}catch(SQLException sqle) {}
+			if(pstmt!=null)
+				try {pstmt.close();}catch(SQLException sqle) {}
+			if(conn!=null)
+				try {conn.close();}catch(SQLException sqle) {}
+		}
+		return x;
 	}
 }
